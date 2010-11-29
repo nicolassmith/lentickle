a91273e4137dd0beaf2362c99c9f29095dc52c2f
nicolassmith <nicolas.d.smith@gmail.com>
2010-11-29 17:23:51 -0500

    added some lines to setupLentickle.m with instructions on getting dependent files:

--- setupLentickle.m
+++ setupLentickle.m
@@ -6,3 +6,10 @@ addpath('OptickleEligo');
 addpath(genpath('mattlib'));
 addpath(genpath('pickleCode'));
 
+% also my useful matlab scripts, available here:
+% git clone git://github.com/nicolassmith/nicmatlabscripts.git
+% or
+% https://github.com/nicolassmith/nicmatlabscripts/zipball/master
+%
+
+%addpath('pathto/nicmatlabscripts')
