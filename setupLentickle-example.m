9a675d2bd7b586fccf83c776a89244f3798651ea
nicolassmith <nicolas.d.smith@gmail.com>
2011-07-29 16:42:50 -0400

    hoping to reduce dependencies, change setupLentickle

--- setupLentickle-example.m
+++ setupLentickle-example.m
@@ -1,10 +1,9 @@
 % sets up environment for lentickle
 
 % set up path
-addpath(genpath('~/ligo/sim/Optickle'));
-addpath('OptickleEligo');
+addpath('~/ligo/sim/Optickle');
 addpath(genpath('mattlib'));
-addpath(genpath('pickleCode'));
+%addpath(genpath('pickleCode'));
 
 % also my useful matlab scripts, available here:
 % git clone git://github.com/nicolassmith/nicmatlabscripts.git
