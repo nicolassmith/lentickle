10c3d05b9ad9ee7da3aaf286814ad3fb16557ee0
Nicolas de Mateo Smith <nicolas@mint-square.mit.edu>
2011-08-02 12:14:16 -0400

    just some changes of the test script

--- exampleMICH.m
+++ exampleMICH.m
@@ -5,7 +5,7 @@ setupLentickle;
 
 opt = exampleMICHopt();
 cucumber = exampleMICHcucumber(opt);
-f = logspace(0,4,500).';
+f = logspace(-2,4,1000).';
 
 results = lentickleEngine(cucumber,[],f);
 
@@ -15,8 +15,38 @@ TF = [f,pickleTF(results,'AM','AS_Q','cl')];
 
 %SRSbode(TF)
 
-COMM_OLG = [f,1-1./pickleTF(results,'COMM','COMM','cl')];
-DIFF_OLG = [f,1-1./pickleTF(results,'DIFF','DIFF','cl')];
+COMM_OLG = 1-1./pickleTF(results,'COMM','COMM','cl');
+DIFF_OLG = 1-1./pickleTF(results,'DIFF','DIFF','cl');
 
+figure(1)
+clf
+zplotlog(f,DIFF_OLG,'b')
+hold on
+subplot(2,1,2)
+hold on
+zplotlog(f,COMM_OLG,'r')
+legend('DIFF','COMM')
 
-SRSbode(DIFF_OLG)
+DIFF2ASQ = pickleTF(results,'DIFF','AS_Q','cl');
+DIFF2ASI = pickleTF(results,'DIFF','AS_I','cl');
+
+figure(2)
+clf
+zplotlog(f,DIFF2ASQ,'b')
+hold on
+subplot(2,1,2)
+hold on
+zplotlog(f,DIFF2ASI,'r')
+legend('ASQ','ASI')
+
+COMM2REFLQ = pickleTF(results,'COMM','REFL_Q','cl');
+COMM2REFLI = pickleTF(results,'COMM','REFL_I','cl');
+
+figure(3)
+clf
+zplotlog(f,COMM2REFLQ,'b')
+hold on
+subplot(2,1,2)
+hold on
+zplotlog(f,COMM2REFLI,'r')
+legend('REFLQ','REFLI')
