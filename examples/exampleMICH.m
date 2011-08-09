7bb0995ddd3e899690b9eac9be7d93225b413797
nicolassmith <nicolas.d.smith@gmail.com>
2011-08-09 18:52:48 -0400

    start commenting example

--- examples/exampleMICH.m
+++ examples/exampleMICH.m
@@ -1,19 +1,43 @@
+%% Power Recycled Michelson Lentickle Example
 % script to calculate loop gains and noise transfer functions for the MICH
 % example.
 
-run ../setupLentickle;
+%% Set up paths
+run ../setupLentickle; 
 
-opt = exampleMICHopt();
-cucumber = exampleMICHcucumber(opt);
-f = logspace(0,4,500).';
+%% Interferometer Model
+% first we will set up our interferometer model, including the optickle
+% plant (handled by Optickle) and the rest of the control system (handled
+% by lentickle).
 
-results = lentickleEngine(cucumber,[],f);
+opt = exampleMICHopt(); % create the opt object
+cucumber = exampleMICHcucumber(opt); % create the cucumber structure
+                                     % look at exampleMICHcucumber.m for
+                                     % more info about the cucumber.
+f = logspace(0,4,500).'; % choose the frequency array we will use
 
+%% Closed loop results
+% We will now call the lentickleEngine function to calculate the closed
+% loop transfer functions of the control system. As arguments, it takes the
+% cucumber, a 'pos' array which has offsets of all the Optickle drives
+% (note: these are 'drives' not 'mirrors'), and the frequency array.
 
-COMM2REFLI = pickleTF(results,'COMM_ctrl','REFL_I','ol');
+results = lentickleEngine(cucumber,[],f); % calculate all results
 
-figure(3)
-zplotlog(f,COMM2REFLI,'r')
+%% Transfer functions
+% All the transfer functions can be extracted from the results structure,
+% and the pickleTF function makes that easy.
+
+%% Open Loop Gains
+% We will calculate the open loop gains of our two degrees of freedom, the
+% differential and common modes of the arms, 'DIFF' and 'COMM'. pickleTF
+% will easily give us the closed loop gain, and calulating the open loop
+% gain from that is fairly easy. (OLG = 1 - 1./CLG)
+
+COMM2REFLI = pickleTF(results,'COMM_ctrl','REFL_I');
+
+zplotlog(f,COMM2REFLI)
+
 
 % measurements to display
 % Loop gain TFs
