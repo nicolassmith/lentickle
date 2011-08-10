4688c81e38197473e58144eb392352d2b974b74f
nicolassmith <nicolas.d.smith@gmail.com>
2011-08-10 15:24:48 -0400

    create github pages branch

--- mf/filtGain.m
+++
@@ -1,9 +0,0 @@
-%
-% mf = filtZPG(mf, g, f)
-%
-% returns mf with gain g at frequency f (in Hz)
-%
-
-function mf = filtZPG(mf, g, f)
-
-  mf.k = mf.k * g / abs(sresp(mf, f));
--- mf/filtInv.m
+++
@@ -1,9 +0,0 @@
-%
-% mf = filtInv(mf)
-%
-% returns the inverse of mf
-%
-
-function mf = filtInv(mf)
-
-  mf = struct('z', mf.p, 'p', mf.z, 'k', 1 / mf.k);
