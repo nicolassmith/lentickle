926a248ae1db7a1b76d2d899e84724d3feb3164d
nicolas <nicolas@gouy.(none)>
2013-08-13 18:02:36 -0700

    placeholder for lock script

---
+++ lentickleLock.m
@@ -0,0 +1,8 @@
+function lockPos = lentickleLock(cucumber,errSetPoint,startPos)
+    % This function locks the error signals in the DOF basis that are given
+    
+    % wrap around setOperatingPoint
+    
+    return lockPos
+    
+end
